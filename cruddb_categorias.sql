8065195e17b7f08b05548c28885cdf2e48dd5ed9
MariaTT <108082130+mriatorres@users.noreply.github.com>
2025-11-08 11:46:08 -0500

    Create cruddb_categorias.sql

---
+++ cruddb_categorias.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categorias` (
+  `id_categoria` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(64) NOT NULL,
+  `descripcion` text,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- Dumping data for table `categorias`
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- Dump completed on 2025-11-08  9:25:27
